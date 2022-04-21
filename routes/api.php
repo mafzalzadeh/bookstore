a3fa796c8df72b59c95b765008139917710a37c2
mahmood <mafzalzadeh@yahoo.com>
2022-04-21 12:54:46 +0430

    Deny Guest access to post book

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 Route::get('/books', 'BooksController@getCollection');
 
-Route::post('/books', 'BooksController@post')->middleware('auth.admin');
+Route::post('/books', 'BooksController@post')->middleware(['auth', 'auth.admin']);
 
 Route::post('/books/{book}/reviews', 'BooksController@postReview');
 
